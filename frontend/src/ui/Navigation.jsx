eeb756e6f07e175e388d71a47950ccf8a5ff9b39
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-04-08 12:02:18 +0100

    12:02 completed the card component

---
+++ frontend/src/ui/Navigation.jsx
@@ -0,0 +1,12 @@
+// Card.jsx
+import React from 'react';
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <h2>Navbar</h2>
+    </div>
+  );
+}
+
+export default Navigation;
